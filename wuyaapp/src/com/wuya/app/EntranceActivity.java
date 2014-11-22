478b1bb02f5735b21c55415f43cae2c37375bf12
xiaocilin <xiaolinjava@163.com>
2014-11-22 14:38:18 +0800

    添加入口页面

---
+++ wuyaapp/src/com/wuya/app/EntranceActivity.java
@@ -0,0 +1,22 @@
+package com.wuya.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 登录，注册的入口页
+ * 
+ * @author xiaocilin
+ * 
+ * @since 20140-11-22
+ * 
+ */
+public class EntranceActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_entrance);	
+	
+	}
+}
--- wuyaapp/src/com/wuya/app/MainActivity.java
+++ wuyaapp/src/com/wuya/app/MainActivity.java
@@ -1,6 +1,10 @@
 package com.wuya.app;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,10 +18,19 @@ public class MainActivity extends Activity {
 		/** 全屏设置，隐藏窗口所有装饰 */
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
 		/** 标题是属于View的，所以窗口所有的修饰部分被隐藏后标题依然有效 */
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		
+
 		setContentView(R.layout.activity_main);
+
+		final Intent entranceIntent = new Intent(this, EntranceActivity.class);
+		Timer timer = new Timer();
+		TimerTask tast = new TimerTask() {
+			@Override
+			public void run() {
+				startActivity(entranceIntent);
+			}
+		};
+		timer.schedule(tast, 3000);
 	}
 }
