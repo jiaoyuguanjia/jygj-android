0bab75e49b968d24945fb50105530a10b9d48412
xiaocilin <xiaolinjava@163.com>
2014-11-22 17:30:32 +0800

    add student register event

--- wuyaapp/src/com/wuya/app/EntranceActivity.java
+++ wuyaapp/src/com/wuya/app/EntranceActivity.java
@@ -1,7 +1,11 @@
 package com.wuya.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * 登录，注册的入口页
@@ -11,12 +15,32 @@ import android.os.Bundle;
  * @since 20140-11-22
  * 
  */
-public class EntranceActivity extends Activity {
+public class EntranceActivity extends Activity implements OnClickListener {
 
+	private Button stuRegisterBtn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_entrance);	
-	
+		
+		stuRegisterBtn = (Button) this.findViewById(R.id.student_register);
+		stuRegisterBtn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.student_register:
+			startActivity(new Intent(this, StudentRegisterActivity.class));
+			break;
+		case R.id.tutor_register:
+			break;
+		case R.id.login:
+			break;
+		default:
+			break;
+		}
+		
 	}
 }
--- wuyaapp/src/com/wuya/app/MainActivity.java
+++ wuyaapp/src/com/wuya/app/MainActivity.java
@@ -6,20 +6,12 @@ import java.util.TimerTask;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		/** 全屏设置，隐藏窗口所有装饰 */
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		/** 标题是属于View的，所以窗口所有的修饰部分被隐藏后标题依然有效 */
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 
 		setContentView(R.layout.activity_main);
 
