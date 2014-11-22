4466a892cdae78132a3eb9ac8bb98c8e05ead4cd
unknown <shengwei@NHSHITNB74722.yihaodian.com>
2014-11-22 16:56:21 +0800

    学生注册页面

---
+++ wuyaapp/src/com/wuya/app/StudentRegisterActivity.java
@@ -0,0 +1,17 @@
+package com.wuya.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by shengwei on 2014/11/22.
+ */
+public class StudentRegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_register);
+    }
+}
