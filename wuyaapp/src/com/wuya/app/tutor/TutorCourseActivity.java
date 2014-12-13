ca89e057d922bfa7a1c2859f20c32ac471569eab
xiaocilin <xiaolinjava@163.com>
2014-12-13 15:46:05 +0800

    add main tutor activity framework from sw

---
+++ wuyaapp/src/com/wuya/app/tutor/TutorCourseActivity.java
@@ -0,0 +1,20 @@
+package com.wuya.app.tutor;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.wuya.app.R;
+
+/**
+ * Created by shengwei on 2014/12/13.
+ */
+public class TutorCourseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutor_course_activity);
+    }
+}
---
+++ wuyaapp/src/com/wuya/app/tutor/TutorPrivacyActivity.java
@@ -0,0 +1,22 @@
+package com.wuya.app.tutor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import com.wuya.app.R;
+
+/**
+ * Created by shengwei on 2014/12/13.
+ */
+public class TutorPrivacyActivity extends Activity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutor_privacy_activity);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
