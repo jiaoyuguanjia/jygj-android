c24071d6ac477a6a3970b9122678916ec31871fa
xiaocilin <xiaolinjava@163.com>
2014-12-28 10:42:27 +0800

    add tutor resume

---
+++ wuyaapp/src/com/wuya/app/tutor/TutorResumeActivity.java
@@ -0,0 +1,16 @@
+package com.wuya.app.tutor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.wuya.app.R;
+
+/**
+ * Created by shengwei on 2014/12/28.
+ */
+public class TutorResumeActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutor_resume_activity);
+    }
+}
