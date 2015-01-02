0bab6e54df4216fead270e1caea311a7880c6d1f
xiaocilin <xiaolinjava@163.com>
2015-01-02 12:13:44 +0800

    #N/A [xiaocilin] 注册Activity

--- wuyaapp/src/com/wuya/app/tutor/TutorTimingActivity.java
+++ wuyaapp/src/com/wuya/app/tutor/TutorTimingActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class TutorTiming extends Activity implements OnClickListener {
+public class TutorTimingActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
