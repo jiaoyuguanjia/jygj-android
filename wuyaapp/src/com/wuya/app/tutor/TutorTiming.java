32711b08b6ec617ee61fab4d820af10a052c5fc7
zhouliangui <zhouliangui@hotmail.com>
2015-01-02 12:04:47 +0800

    tutor timing

---
+++ wuyaapp/src/com/wuya/app/tutor/TutorTiming.java
@@ -0,0 +1,23 @@
+package com.wuya.app.tutor;
+
+import com.wuya.app.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class TutorTiming extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutor_timing);
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
