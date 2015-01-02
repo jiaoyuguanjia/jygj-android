e814aaf8d5f19a03675d489e41d5a2072334d592
xiaocilin <xiaolinjava@163.com>
2015-01-02 17:16:51 +0800

    a

--- wuyaapp/src/com/wuya/app/CourseSearchActivity.java
+++ wuyaapp/src/com/wuya/app/CourseSearchActivity.java
@@ -3,8 +3,16 @@ package com.wuya.app;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * 课程搜索列表页
+ * 
+ * @author xiaocilin
+ * 
+ * @since 2015-1-2
+ * 
+ */
 public class CourseSearchActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
